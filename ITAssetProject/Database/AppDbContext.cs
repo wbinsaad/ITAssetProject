d9bf30a36eb29390af24e1b82be296674805774a
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-06 21:50:58 +0300

    Add DbContext and Models
    - Add dbcontext to connect the application with Database
    - Add ITAsset Model

---
+++ ITAssetProject/Database/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using ITAssetProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ITAssetProject.Database
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("name=ConnectionStrings:DefaultConnection");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ITAsset>(item =>
+            {
+                // Is Unique & Index
+                item.HasIndex(n => n.SerialNumber).IsUnique(true);
+
+
+                // Enums
+                item.Property(n => n.Status).HasConversion<EnumToStringConverter<StatusEnum>>();
+
+
+                // Max Length
+                item.Property(n => n.SerialNumber).HasMaxLength(128).IsRequired(true);
+                item.Property(n => n.Brand).HasMaxLength(128).IsRequired(true);
+                item.Property(n => n.Status).HasMaxLength(128).IsRequired(true);
+            });
+        }
+
+        public DbSet<ITAsset> ITAsset { get; set; }
+    }
+}
---
+++ ITAssetProject/Models/ITAsset.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ITAssetProject.Models
+{
+    public class ITAsset
+    {
+        public Guid Id { get; set; }
+        public string SerialNumber { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public DateTime WarrantyExpirationDate { get; set; }
+
+        [JsonConverter(converterType: typeof(JsonStringEnumConverter))]
+        public StatusEnum Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public enum StatusEnum
+    {
+        New,
+        InUse,
+        Damaged,
+        Dispose
+    }
+}
