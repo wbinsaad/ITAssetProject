4dfd3a75dc2a25d4358163d5c3eef2ac5e16e8c3
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-06 22:14:38 +0300

    Add Unit Test
    - UnitTestAssetController
    - UnitTestDashboardController

---
+++ UnitTestITAsset/UnitTestAssetController.cs
@@ -0,0 +1,83 @@
+﻿using ITAssetProject.Models;
+using ITAssets.Repository;
+using ITAssetsProject.Controllers.ApiControllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace UnitTestITAsset
+{
+    public class UnitTestAssetController
+    {
+        [Fact]
+        public async Task TestGetAllAsync()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.GetAllAsync()).ReturnsAsync(new List<ITAsset>());
+            var assetController = new AssetController(mockITAssetRepository.Object);
+
+            OkObjectResult result = (OkObjectResult)await assetController.Get();
+
+            Assert.Equal(result.StatusCode, 200);
+        }
+
+        [Fact]
+        public async Task TestGet()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.Get(Guid.Empty)).ReturnsAsync(new ITAsset());
+            var assetController = new AssetController(mockITAssetRepository.Object);
+
+            var result = (OkObjectResult)await assetController.Get(Guid.Empty);
+
+            Assert.Equal(result.StatusCode, 200);
+        }
+
+        [Fact]
+        public async Task TestAdd()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.Add(new ITAsset())).ReturnsAsync(new ITAsset());
+            mockITAssetRepository.Setup(service => service.IsSerialNumberExist(string.Empty)).ReturnsAsync(false);
+            mockITAssetRepository.Setup(service => service.SaveChanges()).Returns(Task.CompletedTask);
+
+            var assetController = new AssetController(mockITAssetRepository.Object);
+
+            var result = (OkObjectResult)await assetController.Post(new ITAsset());
+
+            Assert.Equal(200, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestDelete()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.Delete(Guid.Empty));
+            mockITAssetRepository.Setup(service => service.SaveChanges()).Returns(Task.CompletedTask);
+
+            var assetController = new AssetController(mockITAssetRepository.Object);
+
+            var result = (NoContentResult)await assetController.Delete(Guid.Empty);
+
+            Assert.Equal(204, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestEdit()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.Update(Guid.Empty, new ITAsset()));
+            mockITAssetRepository.Setup(service => service.SaveChanges()).Returns(Task.CompletedTask);
+
+            var assetController = new AssetController(mockITAssetRepository.Object);
+
+            var result = (NoContentResult)await assetController.Put(Guid.Empty, new ITAsset());
+
+            Assert.Equal(204, result.StatusCode);
+        }
+    }
+}
---
+++ UnitTestITAsset/UnitTestDashboardController.cs
@@ -0,0 +1,43 @@
+﻿using ITAssetProject.ViewModels.Dashboard;
+using ITAssets.Repository;
+using ITAssetsProject.Controllers.ApiControllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestITAsset
+{
+    public class UnitTestDashboardController
+    {
+        [Fact]
+        public async Task TestGetAssetsByBrand()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.GetAssetsByBrand()).ReturnsAsync(new List<AssetByBrand>());
+            var assetController = new DashboardController(mockITAssetRepository.Object);
+
+            OkObjectResult result = (OkObjectResult)await assetController.GetAssetsByBrand();
+
+            Assert.Equal(result.StatusCode, 200);
+        }
+
+
+        [Fact]
+        public async Task TestGetAssetsByStatus()
+        {
+            var mockITAssetRepository = new Mock<ITAssetRepository>();
+
+            mockITAssetRepository.Setup(service => service.GetAssetsByStatus()).ReturnsAsync(new List<AssetByStatus>());
+            var assetController = new DashboardController(mockITAssetRepository.Object);
+
+            OkObjectResult result = (OkObjectResult)await assetController.GetAssetsByStatus();
+
+            Assert.Equal(result.StatusCode, 200);
+        }
+    }
+}
