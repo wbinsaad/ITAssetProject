09c3c46c491b0937f46a85a1d09f61ab96965035
Waleed BinSaad <wbinsaad@stu.kau.edu.sa>
2024-08-06 21:56:24 +0300

    Add IITAssetRepository as Scoped Service

--- ITAssetProject/Program.cs
+++ ITAssetProject/Program.cs
@@ -1,7 +1,13 @@
+using ITAssetProject.Database;
+using ITAssetProject.Repository;
+using ITAssets.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IITAssetRepository, ITAssetRepository>();
 
 var app = builder.Build();
 
@@ -9,19 +15,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
